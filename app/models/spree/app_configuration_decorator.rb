8c0e700a146c4ea4a3bfcbb45bf9f29d249211ab
Johnny Chan <johnny@Johnnys-MacBook-Pro-2.local>
2020-03-25 16:42:10 +1100

    changed format of config decorator

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,25 +1,39 @@
 module Spree
   module AppConfigurationDecorator
-    # Default mail headers settings
-    preference :mails_from, :string, default: 'spree@example.com'
-    preference :enable_mail_delivery, :boolean, default: false
-    preference :mail_bcc, :string, default: 'spree@example.com'
-    preference :intercept_email, :string, default: nil
+    def self.prepended(base)
+      # Default mail headers settings
+      base.preference :mails_from, :string, default: 'spree@example.com'
+      base.preference :enable_mail_delivery, :boolean, default: false
+      base.preference :mail_bcc, :string, default: 'spree@example.com'
+      base.preference :intercept_email, :string, default: nil
 
-    # Default smtp settings
-    preference :mail_host, :string, default: 'localhost'
-    preference :mail_domain, :string, default: 'localhost'
-    preference :mail_port, :integer, default: 25
-    preference :secure_connection_type, :string, default: Core::MailSettings::SECURE_CONNECTION_TYPES[0]
-    preference :mail_auth_type, :string, default: Core::MailSettings::MAIL_AUTH[0]
-    preference :smtp_username, :string, default: nil
-    preference :smtp_password, :string, default: nil
+      # Default smtp settings
+      base.preference :mail_host, :string, default: 'localhost'
+      base.preference :mail_domain, :string, default: 'localhost'
+      base.preference :mail_port, :integer, default: 25
+      base.preference :secure_connection_type, :string, default: Core::MailSettings::SECURE_CONNECTION_TYPES[0]
+      base.preference :mail_auth_type, :string, default: Core::MailSettings::MAIL_AUTH[0]
+      base.preference :smtp_username, :string, default: nil
+      base.preference :smtp_password, :string, default: nil
+    end
 
     def override_actionmailer_config
       raise 'override_actionmailer_config has been removed. ' \
             'actionmailer\'s config is always overwridden when spree_mail_settings is included'
     end
     alias_method :override_actionmailer_config=, :override_actionmailer_config
+  end
+end
+
+
+
+module Spree
+  module AppConfigurationDecorator
+    def self.prepended(base)
+      base.preference :allow_currency_change,  :boolean, default: false
+      base.preference :show_currency_selector, :boolean, default: false
+      base.preference :supported_currencies,   :string,  default: 'USD'
+    end
   end
 end
 
